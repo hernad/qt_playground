ef441e2793f96c79bc3e1ba4b290f3c94f40ac33
Ernad Husremovic <hernad@bring.out.ba>
2011-09-28 12:27:26 +0200

    hello_tr

---
+++ translation/hello_tr.pro
@@ -0,0 +1,18 @@
+# http://doc.qt.nokia.com/stable/qmake-tutorial.html
+
+# http://redmine.bring.out.ba/issues/24591
+
+TEMPLATE=app
+INCLUDEPATH+=.
+#INCLUDEPATH+=/Library/PostgreSQL/9.1/include
+#LIBS+=/Library/PostgreSQL/9.1/lib/libpq.a
+SOURCES=main.cpp
+
+# HEADERS += hello.h
+
+CONFIG += qt debug
+
+# QT+=sql
+
+
+TRANSLATIONS = hello_tr_bs_BA.ts
---
+++ translation/main.cpp
@@ -0,0 +1,80 @@
+#include <Qt>
+#include <QApplication>
+#include <QTranslator>
+#include <QTextCodec>
+#include <QDebug>
+#include <QDate>
+#include <iostream>
+
+using namespace::std;
+
+int main(int argc, char *argv[])
+{
+     QApplication app(argc, argv);
+
+     QTranslator translator;
+     
+     
+	 QLocale::setDefault(QLocale::German);
+
+	 QLocale german;
+
+	 //QString locale("ba");
+	 //QLocale bosnian("ba");
+
+	 //QLocale::setDefault(bosnian);
+
+	 
+	 //QLocale::setDefault(QLocale::Bosnian);
+
+
+
+	 //QString locale = QLocale::system().name();
+	 QString locale = german.name();
+
+	 QString str_tran("hello_tr_" + locale);
+
+	 QString broj_sa_tackom("1234.56");
+	 QString broj_sa_zarez("1234,56");
+
+	 QString nas_datum("16.01.2004");
+
+	 bool ok;
+
+	 qDebug() << broj_sa_tackom << broj_sa_tackom.toDouble(&ok) << ok;
+	 qDebug() << broj_sa_zarez << broj_sa_zarez.toDouble(&ok) << ok;
+	 qDebug() << "ucitavam " << str_tran;
+
+	 QDate danas(QDate::currentDate());
+	 
+	 QDate d = QDate::fromString("2004-01-16", Qt::ISODate);
+	 qDebug() << "2004-01-16" << d;
+
+	 qDebug() << danas;
+	 qDebug() << german.toString(danas, QLocale::LongFormat);
+	 qDebug() << german.toString(danas, QLocale::ShortFormat);
+	
+	 //qDebug() << "lokalno datum" << nas_datum << QDate::fromString(nas_datum, Qt::LocalDate);
+	 //qDebug() << "lokalno datum" << nas_datum << QDate::fromString(nas_datum, Qt::DefaultLocaleLongDate);
+
+	 QLocale::setDefault(QLocale::Bosnian);
+     QLocale bosnian;
+	 qDebug() << bosnian.toString(danas, QLocale::LongFormat);
+	 qDebug() << bosnian.toString(danas, QLocale::ShortFormat);
+	
+
+
+     translator.load(str_tran);	 
+	 //For a translation-aware application a translator object is created, a translation is loaded and the translator object installed into the application.
+     app.installTranslator(&translator);
+
+     // For non-Latin1 strings in the sources you will also need for example:
+     QTextCodec::setCodecForTr(QTextCodec::codecForName("utf8"));
+
+	 qDebug() << translator.translate("ctx_1","hello_string");
+
+	 cout << "--end of program--" << endl;
+
+	 return 0;
+
+}
